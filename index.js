0ab9fe8f2f0ea079989b30a2ac6da931593f3663
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-05-19 10:31:38 +0530

    finally added caching and resizing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var validator = require('validator');
 
-let options = {
+let options_validator = {
   protocols: [
       'http',
       'https',
@@ -16,6 +16,16 @@ let options = {
   allow_protocol_relative_urls: false,
   disallow_auth: false
 };
+
+let cf_header = {
+    cacheTtl: 5,
+    cacheEverything: true,
+    webp: true,
+    avif: true,
+    scrapeShield: true,
+    polish: 'lossy',
+    minify: {'images': true,'video': true,'css': true,'html': true,'javascript': true, 'audio': true},
+}
 
 addEventListener("fetch", (event) => {
   event.respondWith(
@@ -58,31 +68,40 @@ async function handleRequest(request) {
     if (!url.searchParams.get('url')){
       return return404();
     }
+
     var decoded_string = atob(url.searchParams.get('url'));
-    if (!validator.isURL(decoded_string, options)){
+    if (!validator.isURL(decoded_string, options_validator)){
       return return404('Not a valid URL');
     }
     
-    response = await fetch(decoded_string, {
-      cf: {
-        cacheTtl: 5,
-        cacheEverything: true,
-        webp: true,
-        avif: true,
-        scrapeShield: true,
-        polish: 'lossy',
-        minify: {'images': true,'video': true,'css': true,'html': true,'javascript': true, 'audio': true},
-
-      },
-    })
+    response = await fetch(decoded_string, cf_header)
     if (response.ok || response.redirected) {
       var content_type = response.headers.get('content-type').toLowerCase();
       console.log(content_type , 'Proxy', new Date().toLocaleDateString());
 
       if (content_type.includes('image') || content_type.includes('video') || content_type.includes('audio')){
-        response = new Response(response.body, response);
-        response.headers.set('Cache-Control', 'max-age=1500');
-        return response;
+        last_response = new Response(response.body, response);
+        last_response.headers.set('Cache-Control', 'max-age=2000');
+        last_response.headers.set('cf', cf_header);
+
+        if (content_type.includes('image')){
+          let options = { }
+          if (url.searchParams.has('fit')) options.fit = url.searchParams.get('fit')
+          if (url.searchParams.has("width")) options.width = url.searchParams.get("width")
+          if (url.searchParams.has("height")) options.height = url.searchParams.get("height")
+          if (url.searchParams.has("quality")) options.quality = url.searchParams.get("quality")
+
+          const accept = request.headers.get("Accept");
+          if (/image\/avif/.test(accept)) {
+            options.format = 'avif';
+          } else if (/image\/webp/.test(accept)) {
+            options.format = 'webp';
+          }
+          var image_cf_header = cf_header
+          image_cf_header.image = options
+          last_response.headers.set('cf', image_cf_header);
+        }
+        return last_response;
     }}};
   return return404();
 }
